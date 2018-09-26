7d61f359f22c65d1f3ff79e643a1e31645ecb642
wasswa-derick <derick.wasswa@andela.com>
2018-09-26 17:10:44 +0300

    [Feature #155965773] Create the GithubUsersAdapter

---
+++ app/src/main/java/com/rosen/wasswaderick/nairobijavageeks/adapter/MyAppGlideModule.java
@@ -0,0 +1,14 @@
+package com.rosen.wasswaderick.nairobijavageeks.adapter;
+
+import com.bumptech.glide.annotation.GlideModule;
+import com.bumptech.glide.module.AppGlideModule;
+
+/**
+ * Created by Derick W on 25,September,2018
+ * Github: @wasswa-derick
+ * Andela (Kampala, Uganda)
+ */
+
+@GlideModule
+public class MyAppGlideModule extends AppGlideModule {
+}
